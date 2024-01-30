05fae1f004c235db075730d8b8a35051b37b5986
huangfu <huangfu_project@163.com>
2024-01-30 16:59:23 +0800

    1

--- src/main/java/com/three/SynchronizedClassTest.java
+++ src/main/java/com/three/SynchronizedClassTest.java
@@ -10,33 +10,39 @@ public class SynchronizedClassTest {
         Task task = new Task();
         Thread thread1 = new Thread(task, "线程1");
         Thread thread2 = new Thread(task, "线程2");
-
+        long currentTimeMillis = System.currentTimeMillis();
         thread1.start();
         thread2.start();
         thread1.join();
         thread2.join();
+        System.out.println("总耗时:" + (System.currentTimeMillis() - currentTimeMillis));
 
-        System.out.println("最终的计算结果"+task.getCount());
     }
 
     private static class Task implements Runnable {
-        static int count = 0;
         @Override
         public  void run() {
-            Task.addCount();
-        }
-
-        public static  void addCount(){
-            for (int j = 0; j < 100000; j++) {
-                synchronized (SynchronizedClassTest.class) {
-                    count++;
-                }
-
+            try {
+                Task.simulate();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 
-        public int getCount() {
-            return count;
+        public static  void simulate() throws InterruptedException {
+            System.out.println(Thread.currentThread().getName() + ": 开始查询数据库");
+            //模拟耗时
+            Thread.sleep(5000);
+            System.out.println(Thread.currentThread().getName() + ": 查询数据库结束");
+            synchronized (SynchronizedClassTest.class) {
+                System.out.println(Thread.currentThread().getName() + ": 开始修改临界区数据");
+                //修改临界区数据操作 模拟耗时
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + ": 修改临界区数据结束");
+            }
+            System.out.println(Thread.currentThread().getName() + ": 开始执行剩余的逻辑");
+            Thread.sleep(3000);
+            System.out.println(Thread.currentThread().getName() + ": 执行剩余的逻辑成功");
         }
     }
 }
